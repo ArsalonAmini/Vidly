42e9a9fb2569d4093b953d646147d13ef8a33613
Arsalon <aminihajibasa@gmail.com>
2017-08-10 08:07:47 -0700

    NewRentalModels

---
+++ VidlyExample/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VidlyExample.DTO;
+using VidlyExample.Models;
+
+namespace VidlyExample.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext;
+        }
+
+
+        [HttpPost]
+
+    }
+}
